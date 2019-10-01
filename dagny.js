548cb22b6ae6452e56053f68ae34a6c0f51fd0f4
Alan <info@s0s.app>
2019-09-30 21:57:50 -0700

    Recognizes basic questions

--- dagny.js
+++ dagny.js
@@ -25,13 +25,15 @@ process.on('uncaughtException', function(err) {
   console.log(err)
 });
 
+
+var questionWords = []
 client.on('ready', () => {
   var emuChannel = client.channels.get(chan_emu)
   dagny.log("Connected as " + client.user.tag)
 
   dagny.name("Emu")
   dagny.default_reply("...")
-  dagny.keywords("emu ostrich cassowary bird")
+  dagny.keywords("objectivism ayn rand atlas shrugged")
   dagny.rating("G")
   emuji.load_dictionary()
   emuji.load_training_data()
@@ -40,10 +42,40 @@ client.on('ready', () => {
   //console.log(test)
 
   // set discord client "now playing"
-  client.user.setActivity(dagny.play())
+  var currentYear = new Date
+  currentYear.getFullYear
+
+  var nowPlayingText = "Train Simulator " + (currentYear.getFullYear() + 1) 
+  console.log(nowPlayingText)
+  client.user.setActivity(nowPlayingText)
 
   // send greeting to channel
   //dagny.reply(emuChannel, msg="")
+
+  questionWords.push("who")
+  questionWords.push("what")
+  questionWords.push("where")
+  questionWords.push("how")
+  questionWords.push("why")
+  questionWords.push("when")
+  questionWords.push("which")
+  questionWords.push("do")
+  questionWords.push("does")
+  questionWords.push("did")
+  questionWords.push("will")
+  questionWords.push("wont")
+  questionWords.push("would")
+  questionWords.push("could")
+  questionWords.push("should")
+  questionWords.push("can")
+  questionWords.push("have")
+  questionWords.push("may")
+  questionWords.push("am")
+  questionWords.push("are")
+  questionWords.push("is")
+  questionWords.push("was")
+  questionWords.push("were")
+  questionWords.push("please")
 
 })
 
@@ -68,12 +100,10 @@ client.on('message', (receivedMessage) => {
   // Prevent bot from responding to its own messages
   if (receivedMessage.author == client.user) { return } // catch and release
 
-  var msg = receivedMessage.content;
+  var msg = receivedMessage.content
+  var msg_lc = msg.toLowerCase()
   dagny.log(receivedMessage.channel + msg)
 
-	if (receivedMessage.content.includes("emu")) {
-		receivedMessage.react("🐤")
-	}
 
 	// React to all messages and log each reaction
 	//var emuEmoji = emuji.react(receivedMessage.content)
@@ -91,98 +121,51 @@ client.on('message', (receivedMessage) => {
 			for (var i = 0; i < emuEmoji.length; i++) {
 				//console.log(i)
 				//dagny.log(receivedMessage.channel + msg)
-				receivedMessage.react(emuEmoji[i])
+				//receivedMessage.react(emuEmoji[i])
 			}
-		}
-	}
+        }
+        
+        if (isQuestion(msg_lc)) {
+            receivedMessage.react("❔")
+        }
 
-  // Check if the bot's user was tagged in the message
-  // Always reply to messages from any channel
-  if (receivedMessage.isMentioned(client.user)) {
-    // Get acknowledgement message from catbot
-    var direct_input = receivedMessage.content.toLowerCase()
-    var direct_output = "..."
-
-    // Log acknowledgement message
-    var msg = receivedMessage.content.toLowerCase();
-
-    // Really need to modularize this function... (Done!)
-    if (msg.includes("!gif")) {
-      silent = true
-      dagny.gif(receivedMessage.channel, msg);
+        if (msg_lc.includes("john galt")) {
+            receivedMessage.react("🚂")
+        }
+    }
+    
+    if (msg_lc.includes("who is dagny")) {
+        receivedMessage.channel.send("Who is John Galt?")
     }
 
-    if (msg.includes("!sticker")) {
-      silent = true
-      dagny.sticker(receivedMessage.channel, msg);
+    if (msg_lc.includes("who is john galt")) {
+        receivedMessage.channel.send("...")
     }
 
-		if (msg.includes("!learn")) {
-			var aCommand = msg.split(" ") // because it's not lowercase
-      var tmpCommand = ""
-      var commandLoc = 0
-
-			// double iteration has got to be a bad idea
-			// but if it's stupid and it works it's not stupid
-			for (var i = 0; i < aCommand.length; i++) {
-				if (aCommand[i] == "!learn") {
-					commandLoc = i + 1
-				}
-			}
-
-			// get command
-			for (var i = commandLoc; i < aCommand.length; i++) {
-				tmpCommand = tmpCommand + "," + aCommand[i]
-			}
-
-			// replace any remaining spaces with commas and split on comma
-			var cmdSplit = tmpCommand.replace(" ",",").split(",")
-			console.log(tmpCommand.replace(" ",","))
-			var learnEmoji = cmdSplit[1] // first item should be an emoji
-			var learnDefine = ""
-
-			console.log("learnEmoji: " + learnEmoji)
-
-			// build string, skipping first item
-			for (var i = 2; i < cmdSplit.length; i++) {
-				// equals sign is optional in the command so ignore it
-				if (cmdSplit[i] != "=") {
-					// build comma separated definition string
-					learnDefine = learnDefine + "," + cmdSplit[i]
-				}
-			}
-
-			learnDefine = learnDefine.substring(1) // remove first comma
-			var learnFile = "./logs/emoji-learn.txt"
-			//if (!fs.existsSync(learnFile)) { fs.mkdirSync(learnFile) }
-
-			fs.appendFileSync(learnFile, learnEmoji + "\t" + learnDefine + "\n");
-			dagny.log("learned that " + learnEmoji + " = " + learnDefine)
-
-			receivedMessage.channel.send("Emubot learned that " + learnEmoji + " = " + learnDefine)
-		}
-
-		if ((msg.includes("!refresh")) || (msg.toLowerCase().includes("!reload"))) {
-      silent = true
-      emuji.load_dictionary();
-    }
-
-    if (!(silent)) {
-			var banned = false
-			for (channel in banned_channels) {
-				if (banned_channels[channel] == receivedMessage.channel.id) {
-					banned = true
-				}
-			}
-
-			if (!(banned)) {
-	      dagny.reply(receivedMessage.channel, receivedMessage.content)
-			}
-    }
-  } else {
-
-
-	}
 })
 
+function isQuestion(text) {
+    var retVal = false
+    
+    console.log("Is it a question: " + text)
+    var aText = text.split(" ")
+    var firstWord = aText[0]
+    firstWord = firstWord.replace("'","")
+    var lastLetter = text.slice(-1)
+
+    // is in the list of question words
+    for (var word in questionWords) {
+        console.log(word)
+        console.log(" " + firstWord.includes(questionWords[word]))
+        if (firstWord.includes(questionWords[word]) || (firstWord === questionWords[word])) {
+            retVal = true
+        }
+    }
+    
+    if (lastLetter == ".") { retVal = false }
+    if (lastLetter == "?") { retVal = true }
+
+    console.log(retVal)
+    return retVal
+}
 client.login(bot_secret.bot_secret_token)
